7dbca04d50fe730e156d2d911336fc595c1b4898
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2014-08-29 07:14:50 +0900

    シナリオファイルにコメントを入れた

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -19,27 +19,52 @@ describe('PhoneCat App', function() {
     it('should filter the phone list as user types into the search box', function() {
 
       /*
+       protractor
        ElementArrayFinder
        Use as: element.all(locator)
-       「ElementArrayFinderは、エレメントの配列の操作のために使われる
-       　（エレメントが一つだけの場合と区別される）」
+       「ElementArrayFinderは、HTML要素の配列の操作のために使われる
+       　（HTML要素が一つだけの場合と区別される）」
        https://github.com/angular/protractor/blob/master/lib/protractor.js#L69
        */
 
       /*
+       Locator
        ProtractorBy.prototype.repeater
-       「ng-repeat内部のエレメントを見つける」
+       「ng-repeat内部のHTML要素を見つける」
        https://github.com/angular/protractor/blob/master/lib/locators.js#L217
        */
 
       var phoneList = element.all(by.repeater('phone in phones'));
+
+      /*
+       locator
+       ProtractorBy.prototype.model
+       Use as: by.model(modelName)
+       「ng-model表現を持つHTML要素を見つける」
+       https://github.com/angular/protractor/blob/master/lib/locators.js#L140
+       */
+
       var query = element(by.model('query'));
 
       expect(phoneList.count()).toBe(3);
 
+      /*
+       webdriver
+       webdriver.WebElement.prototype.sendKeys
+       「このインスタンスによって表されるDOM要素に対して、いくつかのキーを打ち込む命令を予定する」
+       https://code.google.com/p/selenium/source/browse/javascript/webdriver/webdriver.js#1757
+       */
+
       query.sendKeys('nexus');
       expect(phoneList.count()).toBe(1);
 
+      /*
+       webdriver
+       webdriver.WebElement.prototype.clear
+       「この要素の{@code value}を消去する命令を予定する。
+       　背後ののDOM要素がテキストINPUT要素でもTEXTAREA要素でもない場合、この命令は効果がない。」
+       https://code.google.com/p/selenium/source/browse/javascript/webdriver/webdriver.js#1966
+       */
       query.clear();
       query.sendKeys('motorola');
       expect(phoneList.count()).toBe(2);
