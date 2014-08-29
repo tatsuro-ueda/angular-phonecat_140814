def66375d1dd60c6b96b62e080ae2f6f3f76c0c0
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2014-08-29 09:02:05 +0900

    ステップ3を終了した

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -22,25 +22,44 @@ describe('PhoneCat App', function() {
        protractor
        ElementArrayFinder
        Use as: element.all(locator)
-       「ElementArrayFinderは、HTML要素の配列の操作のために使われる
-       　（HTML要素が一つだけの場合と区別される）」
+       「ElementArrayFinderは、要素の配列の操作のために使われる
+       　（要素が一つだけの場合と区別される）」
        https://github.com/angular/protractor/blob/master/lib/protractor.js#L69
        */
 
       /*
        Locator
        ProtractorBy.prototype.repeater
-       「ng-repeat内部のHTML要素を見つける」
+       「ng-repeat内部の要素を見つける」
        https://github.com/angular/protractor/blob/master/lib/locators.js#L217
        */
 
       var phoneList = element.all(by.repeater('phone in phones'));
 
       /*
+       protractor
+       ElementFinder
+       Use as: element(locator)
+       「ElementFinderは、たいていの場合、Webエレメントとして扱うことができる。
+       　特に、Webエレメントと同じように、ElementFinderに対して
+       　アクション（click、getTextなど）を行うことができる。
+       　ElementFinderはPromiseを継承している。
+       　そのため、複数のElementFinder上でひとたびアクションが行われると、
+       　thenを使うことで、そのチェーンの最後の結果にアクセスすることができる。
+       　Webエレメントとは違い、ElementFinderは
+       　探索やアクションを行う前にangularが完了するのを待つ。
+
+       　ElementFinderは、要素を見つけるためのlocatorのチェーンを作るのに使うことができる。
+       　実際にはElementFinderは、アクションが呼ばれるまで要素を探そうとしない。
+       　つまりそれらは、ページが利用可能になる前に
+       　ヘルパーファイルの中でセットアップすることができる。（？）」
+      */
+
+      /*
        locator
        ProtractorBy.prototype.model
        Use as: by.model(modelName)
-       「ng-model表現を持つHTML要素を見つける」
+       「ng-model表現を持つ要素を見つける」
        https://github.com/angular/protractor/blob/master/lib/locators.js#L140
        */
 
@@ -68,6 +87,16 @@ describe('PhoneCat App', function() {
       query.clear();
       query.sendKeys('motorola');
       expect(phoneList.count()).toBe(2);
+    });
+
+    it('should display the current filter value in the title bar', function() {
+      var query = element(by.model('query'));
+      
+      query.clear();
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+
+      query.sendKeys('nexus');
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
     });
   });
 });
