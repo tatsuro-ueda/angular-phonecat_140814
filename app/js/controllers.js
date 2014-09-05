7eb79f3c33dac90f1ae6c40d4cfc63e40c74875d
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2014-09-05 09:22:33 +0900

    コントローラの中でageとorderPropを設定した

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,15 +11,24 @@ $scopeは、PhoneListCtrlという名のcontrollerのスコープ
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
+     'snippet': 'Fast just got faster with Nexus S.',
+      'age': 1},
     {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
+     'snippet': 'The Next, Next Generation tablet.',
+      'age': 2},
     {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
+     'snippet': 'The Next, Next Generation tablet.',
+      'age': 3}
   ];
+
+  /*
+   実験
   $scope.name = "World";
   $scope.isLong = function(phone) {
   	var n = phone.name.length;
   	return ( n > 7);
   };
+   */
+
+  $scope.orderProp = 'age';
 });
